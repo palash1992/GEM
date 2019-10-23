f0ff366e7f19978e77ceb12b657dcb16a5eb605f
Palash Goyal <palashgo@usc.edu>
2019-10-23 10:58:32 -0700

    gf.cpp to be compiled directly and put the exe in c_exe

---
+++ gem/c_src/gf.cpp
@@ -0,0 +1,190 @@
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <numeric>
+#include <algorithm>
+#include <unistd.h>
+#include <cmath>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <assert.h>
+#include <vector>
+
+int n_nodes;
+int n_edges;
+int **edges;
+float *edge_weights;
+float **X;
+
+void gen_rand_network(int n, int m)
+{
+    int row;
+    n_nodes = n;
+    n_edges = m;
+    edges = (int **)malloc(n_edges*sizeof(int *));
+    for(int i = 0; i < n_edges; i++)
+        edges[i] = (int *)malloc(2*sizeof(int));
+
+    edge_weights = (float *)malloc(n_edges*sizeof(float));
+    assert(edges != NULL);
+    row = 0;
+    while(row < n_edges)
+    {
+        edges[row][0] = rand()%n_nodes;
+        edges[row][1] = rand()%n_nodes;
+        edge_weights[row] = 1.0;
+        row++;
+    }
+}
+
+void init_embedding(int d)
+{
+    X = (float **)malloc(n_nodes*sizeof(float *));
+    for(int i = 0; i < n_nodes; i++)
+        X[i] = (float *)malloc(d*sizeof(float));
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator (seed);
+    std::normal_distribution<float> distribution (0.0,1.0);
+    for (int i = 0; i < n_nodes; i++)
+        for(int j = 0; j < d; j++)
+            X[i][j] = 0.01*distribution(generator);
+}
+
+void load_network(std::string f_name, bool is_weighted)
+{
+    std::ifstream f;
+    int v_i, v_j, row;
+    float w;
+
+    f.open(f_name);
+    f >> n_nodes;
+    f >> n_edges;
+    edges = (int **)malloc(n_edges*sizeof(int *));
+    for(int i = 0; i < n_edges; i++)
+        edges[i] = (int *)malloc(2*sizeof(int));
+
+    edge_weights = (float *)malloc(n_edges*sizeof(float));
+    assert(edges != NULL);
+
+    row = 0;
+    if(is_weighted)
+    {
+        while(f >> v_i  >> v_j >>  w)
+        {
+            edges[row][0] = v_i;
+            edges[row][1] = v_j;
+            edge_weights[row] = w;
+            row++;
+        }
+    }
+    else
+    {
+        while(f >> v_i  >> v_j)
+        {
+            edges[row][0] = v_i;
+            edges[row][1] = v_j;
+            edge_weights[row] = 1.0;
+            row++;
+        }
+    }
+    f.close();
+}
+
+void _print_f_value(int d)
+{
+    float f1 = 0.0, f2 = 0.0;
+    float i_j_dot, w_ij;
+    int v_i, v_j;
+    for(int edge_id = 0; edge_id < n_edges; edge_id++)
+    {
+        v_i = edges[edge_id][0];
+        v_j = edges[edge_id][1];
+        w_ij = edge_weights[edge_id];
+        i_j_dot = 0.0;
+        for(int d_id = 0; d_id < d; d_id++)
+            i_j_dot += X[v_i][d_id]*X[v_j][d_id];
+        f1 += (w_ij - i_j_dot)*(w_ij - i_j_dot);
+    }
+    for(int node_id = 0; node_id < n_nodes; node_id++)
+        for(int d_id = 0; d_id < d; d_id++)
+            f2 += X[node_id][d_id]*X[node_id][d_id];
+    std::cout << "\t\tObjective: " << f1+f2 << ", f1: " << f1 << ", f2:" << f2 << std::endl;
+}
+
+void saveEmbToTxt(std::string of_name, int d)
+{
+    std::ofstream f;
+    f.open(of_name);
+    f << n_nodes << " " << d << std::endl;
+    for(int node_id = 0; node_id < n_nodes; node_id++)
+    {
+        f << node_id;
+        for(int d_id = 0; d_id < d; d_id++)
+            f << " " << X[node_id][d_id];
+        f << std::endl;
+    }
+    f.close();
+
+}
+void learn_embedding(std::string if_name, std::string of_name,
+                     bool verbose, bool is_weighted, int d, float eta,
+                     float regu, int max_iter, int print_step)
+{
+    clock_t t;
+    srand(time(NULL));
+    t = clock();
+    load_network(if_name, is_weighted);
+    // gen_rand_network(1e3, 3e4);
+    init_embedding(d);
+    int v_i, v_j;
+    float i_j_dot, w_ij;
+    for(int iter_id = 0; iter_id < max_iter; iter_id++)
+    {
+        if(verbose)
+        {
+            if(iter_id%print_step == 0)
+            {
+                std::cout << "\tIter id: " << iter_id << std::endl;
+                _print_f_value(d);
+            }
+        }
+        for(int edge_id = 0; edge_id < n_edges; edge_id++)
+        {
+            v_i = edges[edge_id][0];
+            v_j = edges[edge_id][1];
+            w_ij = edge_weights[edge_id];
+            if(v_j <= v_i)
+                continue;
+            i_j_dot = 0;
+            for(int d_id = 0; d_id < d; d_id++)
+                i_j_dot += X[v_i][d_id]*X[v_j][d_id];
+            for(int d_id = 0; d_id < d; d_id++)
+                X[v_i][d_id] -= eta*(regu*X[v_i][d_id] - (w_ij - i_j_dot)*X[v_j][d_id]);
+        }
+    }
+    t = clock() - t;
+    t = clock();
+    saveEmbToTxt(of_name, d);
+}
+
+int main(int argc, char* argv[])
+{
+    std::string if_name, of_name;
+    bool verbose, is_weighted;
+    int d, max_iter, print_step;
+    float eta, regu;
+    if_name = std::string(argv[1]);
+    of_name = std::string(argv[2]);
+    verbose = char(atoi((argv[3])));
+    is_weighted = char(atoi((argv[4])));
+    d = atoi(argv[5]);
+    eta = atof(argv[6]);
+    regu = atof(argv[7]);
+    max_iter = atoi(argv[8]);
+    print_step = atoi(argv[9]);
+    learn_embedding(if_name, of_name, verbose, is_weighted,
+                    d, eta, regu, max_iter, print_step);
+    return 0;
+}
+
